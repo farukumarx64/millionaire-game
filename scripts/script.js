99b677e432ae1f4ad32d997fffe1013b063a85db
farukumarx64 <f.umaridris.mail@gmail.com>
2023-12-14 12:14:16 +0100

    level can be incremented

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,7 @@
+let correctAnswer = undefined;
+let wrongAnswers = undefined;
+
+let PointIndex = 15;
 function loadProgress() {
   const progress = document.querySelector(".progress");
   const points = [
@@ -30,32 +34,139 @@ function loadProgress() {
 }
 
 async function startGame() {
-  console.log('hello')
-  const questionDisplay = document.querySelector('.question h3')
-  let point = document.querySelector('.progress').childNodes[15]
-  point.style.color = 'black';
-  point.style.backgroundColor = '#f76029';
+  console.log("hello");
+  const questionDisplay = document.querySelector(".question h3");
+  const choices = document.querySelectorAll(".answer button");
+  let point = document.querySelector(".progress").childNodes[PointIndex];
+  console.log(document.querySelector(".progress").childNodes);
+  setActive(point);
 
-  const response = await fetch('https://the-trivia-api.com/v2/questions?categories=general_knowledge');
-  const questions = await response.json()
+  let response = await fetch(
+    "https://the-trivia-api.com/v2/questions?categories=general_knowledge"
+  );
+  let questions = await response.json();
   questionDisplay.textContent = questions[0].question.text;
-  const correctAnswer = questions[0].correctAnswer
-  const wrongAnswers = questions[0].incorrectAnswers
-  console.log(questions[0], questions, correctAnswer, wrongAnswers)
-  shuffleAnswers(correctAnswer, wrongAnswers)
+  let correctAnswer = questions[0].correctAnswer;
+  let wrongAnswers = questions[0].incorrectAnswers;
+  shuffleAnswers(correctAnswer, wrongAnswers);
+  console.log(correctAnswer);
 
-
+  choices.forEach((choice) => {
+    choice.addEventListener("click", function () {
+      console.log(correctAnswer, "yay");
+      verifyAnswer(correctAnswer, choice, questionDisplay);
+    });
+  });
 }
 
-function shuffleAnswers (correctAnswer, wrongAnswers) {
-  const choices = document.querySelectorAll('.answer button');
-  choices.forEach(choice => {
-    
-  })
-  console.log(choices[Math.floor(Math.random() * 3)]);
+function verifyAnswer(answer, choice, questionDisplay) {
+  const chosenChoice = choice.textContent.split(": ")[1];
+  console.log(answer, chosenChoice, correctAnswer);
+  if (correctAnswer != undefined) {
+    if (correctAnswer === chosenChoice) {
+      choice.style.backgroundColor = "green";
+      choice.style.borderColor = "white";
+      questionDisplay.textContent = "CORRECT!";
+      nextQuestion();
+      return;
+      // proceed with logic
+    } else {
+      lostGame(choice, questionDisplay);
+    }
+  } else {
+    if (answer === chosenChoice) {
+      choice.style.backgroundColor = "green";
+      choice.style.borderColor = "white";
+      questionDisplay.textContent = "CORRECT!";
+      nextQuestion();
+      return;
+      // proceed with logic
+    } else {
+      setTimeout(resetOption, 3000);
+      lostGame(choice, questionDisplay);
+    }
+  }
+}
 
+function lostGame(choice, questionDisplay) {
+  choice.style.backgroundColor = "red";
+  choice.style.borderColor = "white";
+  questionDisplay.textContent = "WRONG!";
+  window.location.replace("./lose.html");
+}
+async function resetOption() {
+  const choices = document.querySelectorAll(".answer button");
+  choices.forEach((choice) => {
+    choice.style.backgroundColor = "black";
+    choice.style.borderColor = "#1363bc";
+  });
+}
+function nextQuestion() {
+  let point = document.querySelector(".progress").childNodes[PointIndex];
+  removeActive(point);
+  PointIndex--;
+  point = document.querySelector(".progress").childNodes[PointIndex];
+  setActive(point);
+  setTimeout(resetOption, 3000);
+  setTimeout(generateQuestion, 3000);
+  //resetOption();
+  //startGame();
+}
+async function generateQuestion() {
+  console.log("hello");
+  const questionDisplay = document.querySelector(".question h3");
+  let point = document.querySelector(".progress").childNodes[PointIndex];
+  console.log(document.querySelector(".progress").childNodes);
+  setActive(point);
+
+  const response = await fetch(
+    "https://the-trivia-api.com/v2/questions?categories=general_knowledge"
+  );
+  const questions = await response.json();
+  questionDisplay.textContent = questions[0].question.text;
+  correctAnswer = questions[0].correctAnswer;
+  wrongAnswers = questions[0].incorrectAnswers;
+  shuffleAnswers(correctAnswer, wrongAnswers);
+  console.log(correctAnswer);
+}
+function setActive(point) {
+  point.style.color = "black";
+  point.style.backgroundColor = "#f76029";
+}
+function removeActive(point) {
+  point.style.color = "#a2794d";
+  point.style.backgroundColor = "";
+}
+
+function shuffleAnswers(correctAnswer, answers) {
+  answers.push(correctAnswer);
+  answers = shuffle(answers);
+  const option = ["A", "B", "C", "D"];
+  const choices = document.querySelectorAll(".answer button");
+  choices.forEach((choice, index) => {
+    choice.textContent = `${option[index]}: ${answers[index]}`;
+  });
+}
+
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex > 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
 }
 
 loadProgress();
 startGame();
-
